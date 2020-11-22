3addd068d363846a095c43ef23d085496d95e3b6
Sarah Dowd <sarahlynnedowd@gmail.com>
2020-11-22 09:36:13 -0500

    loop through search data and append to dom

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,20 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+}
+//fetch data from api
 var getUserRepos = function(user) {
     //format the guithib api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -5,9 +22,45 @@ var getUserRepos = function(user) {
     //make a request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-        console.log(data);
+        displayRepos(data, user);
     });
     });
+};
+//display data from api
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+    //clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+    //loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        //format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        //create container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        //create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+        //append to container
+        repoEl.appendChild(titleEl);
+        //create a status Element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        //check if currenet repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = 
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + "issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status icon icon-success'></i>";
+        }
+        //append to container
+        repoEl.appendChild(statusEl);
+        //append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
 
 };
-getUserRepos("sldowd");
+userFormEl.addEventListener("submit", formSubmitHandler);
